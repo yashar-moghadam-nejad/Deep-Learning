d8f28101182915c15f1f28ced79be495d213adcf
Yashar <xxyasharmoghadamnejadxx@gmail.com>
2024-12-02 15:49:40 +0330

    changing optimizer to adam

--- first_model.py
+++ first_model.py
@@ -4,11 +4,12 @@ from tensorflow import keras
 # Input data
 x = tf.constant([1, 2, 3, 4], dtype=float)
 y = x * 3 + 5
+
 # Model definition
 model = keras.Sequential([
     keras.layers.Dense(units=1, activation=None, input_shape=[1])
 ])
-model.compile(optimizer='sgd', loss='mean_squared_error')
+model.compile(optimizer='adam', loss='mean_squared_error')
 
 # Model training
 model.fit(x, y, batch_size=1, epochs=500)
