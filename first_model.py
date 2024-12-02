f3857d56da3bb1bc1d33b845dd89dbcd8d1581c6
Yashar <xxyasharmoghadamnejadxx@gmail.com>
2024-12-02 15:36:42 +0330

    final committed

--- first_model.py
+++ first_model.py
@@ -1,21 +1,23 @@
 import tensorflow as tf
 from tensorflow import keras
 
-x = tf.constant([1,2,3,4],dtype = float)
+# Input data
+x = tf.constant([1, 2, 3, 4], dtype=float)
 y = x * 3 + 5
-
-# model definition
+# Model definition
 model = keras.Sequential([
-    keras.layers.Dense(units=1,activation=None, input_shape=[1])
+    keras.layers.Dense(units=1, activation=None, input_shape=[1])
 ])
-model.compile(optimizer='sgd',loss='mean_squared_error')
+model.compile(optimizer='sgd', loss='mean_squared_error')
 
-# model training
-model.fit(x,y,batch_size=1,epochs=500)
+# Model training
+model.fit(x, y, batch_size=1, epochs=500)
 
-# testing the model
-x_input = tf.constant([50,30,200],dtype=float)
+# Testing the model
+x_input = tf.constant([50, 30, 200], dtype=float)
+x_input = tf.reshape(x_input, (-1, 1))  # Reshape to (3, 1)
 y_predict = model.predict(x_input)
 
+# Print the predictions
+print("Predictions for inputs [50, 30, 200]:")
 print(y_predict)
- 
